1aa8678413db3f70fc3b347fab1e88313975be6c
Haider Aftab <121552824+Haidercodehach@users.noreply.github.com>
2024-02-17 19:41:17 +0500

    fg

--- src/app/component/MainHeader.js
+++ src/app/component/MainHeader.js
@@ -1,3 +1,7 @@
 export default function MainHeader() {
-  return <header className="">ul</header>;
+  return (
+    <header className="bg-green w-full h-full">
+      ujygfg yg iyaui ui u au hhghguhghhgh aghghuh
+    </header>
+  );
 }
