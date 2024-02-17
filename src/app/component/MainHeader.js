1df3a8bcb2b639549a8c18e80dff05546785dd62
Haider Aftab <121552824+Haidercodehach@users.noreply.github.com>
2024-02-17 17:34:00 +0500

    start Creating Header

---
+++ src/app/component/MainHeader.js
@@ -0,0 +1,3 @@
+export default function MainHeader() {
+  return <header className="">ul</header>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,10 @@
 import Navbar from "./component/Navebar";
+import MainHeader from "./component/MainHeader";
 export default function Home() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <MainHeader />
+    </>
+  );
 }
