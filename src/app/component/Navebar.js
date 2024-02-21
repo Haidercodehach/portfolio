b8eea08ba4f265dd2248f46279335f546edb668f
Haider Aftab <121552824+Haidercodehach@users.noreply.github.com>
2024-02-21 16:21:13 +0500

    reponsive Navbar

--- src/app/component/Navebar.js
+++ src/app/component/Navebar.js
@@ -1,38 +1,47 @@
 export default function Navbar() {
   return (
-    <nav className="flex item-center justify-between p-4 fixed top-0 z-50">
+    <nav className="flex item-center justify-between p-4 fixed top-0 z-50 max-sm:p-2">
       <ul className="flex items-center justify-between gap-x-8">
         <li>
-          <a className="text-xl font-bold hover:text-gray-600" href="#home">
+          <a
+            className="text-xl font-bold hover:text-gray-600 max-sm:text-lg"
+            href="#home"
+          >
             Home
           </a>
         </li>
         <li>
-          <a className="text-xl font-bold hover:text-gray-600" href="#about">
+          <a
+            className="text-xl font-bold hover:text-gray-600 max-sm:text-lg"
+            href="#about"
+          >
             About
           </a>
         </li>
         <li>
           <a
-            className="text-xl font-bold hover:text-gray-600"
+            className="text-xl font-bold hover:text-gray-600 max-sm:text-lg"
             href="#techStack"
           >
-            Tech Stack
+            Tech
           </a>
         </li>
         <li>
-          <a className="text-xl font-bold hover:text-gray-600" href="#projects">
+          <a
+            className="text-xl font-bold hover:text-gray-600 max-sm:text-lg"
+            href="#projects"
+          >
             Projects
           </a>
         </li>
         <li>
           <a
-            className="text-xl font-bold hover:text-gray-600"
+            className="text-xl font-bold hover:text-gray-600 max-sm:text-lg"
             href="https://docs.google.com/forms/d/e/1FAIpQLSfEjtTPnnpLF6irQWybP6YS9vBLnpRtzMalG5NbwMFoHPXmqw/viewform"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Contact Us
+            Contact
           </a>
         </li>
       </ul>
